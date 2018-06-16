6e5887f1f4801c83a3203c158ad5846c61bc9743
Andrew Kutta <andrew.kutta@emerson.com>
2018-06-16 15:16:07 -0500

    update test to new design - DK

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
-let Node = require('./node').Node;
-let node = new Node('simulator', 'simulator')
-node.rpc('th_thermostat', 'verify')
+let Node = require('./Node').Node;
+let node = new Node('simulator', 'simulator');
+node.connect()
+  .then(conn => conn.rpc('th_thermostat', 'verify'))
   .then(console.log)
   .catch(console.log)
-  .then(process.exit)
+  .then(process.exit);
