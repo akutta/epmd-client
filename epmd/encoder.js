aeac13337812f9072bd723079e431ae3b386a9fd
Andrew Kutta <andrew.kutta@emerson.com>
2018-06-17 23:37:46 -0500

    minor refactors

--- epmd/encoder.js
+++ epmd/encoder.js
@@ -11,43 +11,32 @@ exports.requestWrapper = req => {
 };
 
 exports.requestPort = node => {
-  let baseLength = 2;
   let nameLength = Buffer.byteLength(node, 'utf8') - 1;
-  let req        = new Buffer(baseLength + nameLength);
-  let offset     = 0;
-  req.writeUInt8(constants.PORT_PLEASE2_REQ, offset);
-  offset = 1;
-  req.write(node, offset);
+  let req        = new Buffer(2 + nameLength);
+  req.writeUInt8(constants.PORT_PLEASE2_REQ, 0);
+  req.write(node, 1);
   return req;
 };
 
 exports.sendName = node => {
-  let baseLength = 7;
   let nameLength = Buffer.byteLength(node, 'utf8') - 1;
-  let req        = new Buffer(baseLength + nameLength);
-  let offset     = 0;
+  let req        = new Buffer(7 + nameLength);
 
-  req.writeUInt8(constants.NAMES_REQ, offset);
-  offset = 1;
-
-  req.writeUInt16BE(5, offset);
-  offset += 2;
-
+  req.writeUInt8(constants.NAMES_REQ, 0);
+  req.writeUInt16BE(5, 1);
   req.writeUInt32BE(
     constants.DFLAG_EXTENDED_REFERENCES
     | constants.DFLAG_EXTENDED_PIDS_PORTS
     | constants.DFLAG_BIT_BINARIES
     | constants.DFLAG_NEW_FLOATS,
-    offset);
-  offset += 4;
-
-  req.write(node, offset);
+    3);
+  req.write(node, 7);
 
   return this.requestWrapper(req);
 };
 
 exports.sendChallenge = (digest, challenge) => {
-  let req = new Buffer(5); // 5
+  let req = new Buffer(5);
   req.writeUInt8(constants.SEND_CHALLENGE_REPLY, 0);
   req.writeUInt32BE(challenge, 1);
   debug('send_challenge_reply: ', this.requestWrapper(Buffer.concat([req, digest])));
